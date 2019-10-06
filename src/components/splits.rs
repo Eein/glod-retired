275e1d2b22987242894fb25129124001f16476a7
William Volin <me@williamvolin.com>
2019-10-05 23:40:57 -0700

    optimizes splits data access and splits up init and redraw

--- src/components/splits.rs
+++ src/components/splits.rs
@@ -1,8 +1,8 @@
 use livesplit_core::component::splits::{
-  Component, 
-  Settings, 
-  ColumnSettings, 
-  ColumnStartWith, 
+  Component,
+  Settings,
+  ColumnSettings,
+  ColumnStartWith,
   ColumnUpdateWith,
   ColumnUpdateTrigger,
 };
@@ -30,156 +30,105 @@ use crate::state::State;
 pub struct Splits {
   component: Component,
   settings: Settings,
+  split_rows: Vec<gtk::Box>,
+  split_names: Vec<gtk::Label>,
+  split_columns: Vec<Vec<gtk::Label>>, // indexes = [row][column]
   pub widget: gtk::ScrolledWindow,
+  viewport: gtk::Viewport,
+  container: gtk::Box,
 }
 
 // https://docs.rs/livesplit-core/0.11.0/livesplit_core/component/splits/index.html
-
-
-// Things to try:
-// selecting the current widget by index and running an update against ath
-
-// TODO: create an update_split method that is iterated on by 
-// ContainerExt.foreach(|c| {}). This should update the times present
-// in the state without destroying the times. After this, also revise the timer
-// to not destroy and update its time internally as well.
-// Performance may vary by update vs delete and create - but worth trying
-// updates first as its non-desctructive and shouldn't destroy scrolling behavior.
 
 impl Splits {
   pub fn new(state: &State) -> Splits {
     let settings = Splits::default_settings();
     let mut component = Component::with_settings(settings.clone());
-    let widget = Splits::widget(&mut component, &state);
-    Splits {
-      settings,
-      component,
-      widget,
-    }
-  }
+    let split_columns = Splits::init_split_columns(&mut component, &state);
+    let split_names = Splits::init_split_names(&mut component, &state);
+    let split_rows = Splits::init_split_rows(&mut component, &state, &split_columns);
 
-  pub fn redraw(&mut self, state: &State) {
-    // Look into possible removing the destroy step in 
-    // favor of changing the nested widgets
-    // This is nasty here, need to find out how to select through
-    if let Some(vp) = self.widget.get_child() {
-      if let Some(viewbox) = vp.downcast_ref::<gtk::Viewport>() {
-        if let Some(child) = viewbox.get_child() {
-          child.destroy();
-        }
-      }
-      // vp.destroy();
-    }
-
-    let container = gtk::Box::new(Orientation::Vertical, 0);
-    gtk::WidgetExt::set_name(&container, "splits-container");
-    container.get_style_context().add_class("splits-container");
-
-    for s in &self.component.state(&state.timer.read(), &state.general_layout_settings).splits {
-      // -- css --
-      let split = gtk::Box::new(Orientation::Horizontal, 0);
-      if s.is_current_split == true {
-        split.get_style_context().add_class("current-split");
-      }else{
-        if s.index % 2 == 0 {
-          split.get_style_context().add_class("even");
-        } else {
-          split.get_style_context().add_class("odd");
-        }
-      }
-      split.get_style_context().add_class("split-container");
-      // -- fields --
-      let name = gtk::Label::new(None);
-      name.set_text(&s.name);
-      split.add(&name);
-
-      // Render each ColumnState
-      for c in s.columns.iter() {
-        // live segment delta
-
-        let column = gtk::Label::new(None);
-        column.set_text(&c.value);
-        &split.pack_end(&column, false, false, 5);
-
-        // TODO: For deltas, feels like theres a better alternative...
-        if let Some(split_index) = &state.timer.read().current_split_index() {
-          let delta_label = gtk::Label::new(None);
-          if s.is_current_split {
-            let delta = livesplit_core::analysis::state_helper::live_segment_delta(
-              &state.timer.read(),
-              s.index,
-              "Personal Best",
-              livesplit_core::TimingMethod::RealTime
-            );
-            let formatted = TimeSpanFormatter::to_delta(delta);
-            delta_label.set_text(&formatted);
-          } else if &s.index < split_index {
-            let delta = livesplit_core::analysis::state_helper::previous_segment_delta(
-              &state.timer.read(),
-              s.index,
-              "Personal Best",
-              livesplit_core::TimingMethod::RealTime
-            );
-            let formatted = TimeSpanFormatter::to_delta(delta);
-            delta_label.set_text(&formatted);
-          }
-          &split.pack_end(&delta_label, false, false, 5);
-        }
-      }
-
-      container.add(&split);
-    }
-
-    if let Some(vp) = self.widget.get_child() {
-      if let Some(viewbox) = vp.downcast_ref::<gtk::Viewport>() {
-          viewbox.add(&container);
-      }
-    }
-    self.widget.show_all();
-  }
-
-  pub fn widget(component: &mut Component, state: &State) -> gtk::ScrolledWindow {
-    let window = ScrolledWindow::new(NONE_ADJUSTMENT, NONE_ADJUSTMENT);
-    window.set_propagate_natural_height(true);
-    window.set_overlay_scrolling(true);
+    let widget = ScrolledWindow::new(NONE_ADJUSTMENT, NONE_ADJUSTMENT);
+    widget.set_propagate_natural_height(true);
+    widget.set_overlay_scrolling(true);
 
     let viewport = Viewport::new(NONE_ADJUSTMENT, NONE_ADJUSTMENT);
     let container = gtk::Box::new(Orientation::Vertical, 0);
     gtk::WidgetExt::set_name(&container, "splits-container");
     container.get_style_context().add_class("splits-container");
 
+    for (index, r) in split_rows.iter().enumerate() {
+      let name = &split_names[index];
+      let columns = &split_columns[index];
+      r.add(name);
+      for c in columns {
+        r.add(c);
+      }
+      container.add(r);
+    }
+
+    viewport.add(&container);
+    widget.add(&viewport);
+
+    Splits {
+      settings,
+      component,
+      widget,
+      viewport,
+      container,
+      split_rows,
+      split_names,
+      split_columns,
+    }
+  }
+
+  pub fn init_split_rows(component: &mut Component, state: &State, split_columns: &Vec<Vec<gtk::Label>>) -> Vec<gtk::Box> {
+    let mut rows: Vec<gtk::Box> = Vec::new();
     for s in &component.state(&state.timer.read(), &state.general_layout_settings).splits {
-      // -- css --
-      // -- fields --
+      let row = gtk::Box::new(Orientation::Horizontal, 0);
+      rows.push(row);
+    }
+    rows
+  }
+
+  pub fn init_split_names(component: &mut Component, state: &State) -> Vec<gtk::Label> {
+    let mut names: Vec<gtk::Label> = vec![];
+    for s in &component.state(&state.timer.read(), &state.general_layout_settings).splits {
       let name = gtk::Label::new(None);
       name.set_text(&s.name);
-
-      let split = gtk::Box::new(Orientation::Horizontal, 0);
-      if s.is_current_split == true {
-        split.get_style_context().add_class("current-split");
-      }
-      if s.index % 2 == 0 {
-        split.get_style_context().add_class("even");
-      } else {
-        split.get_style_context().add_class("odd");
-      }
-      split.get_style_context().add_class("split-container");
-
-      split.add(&name);
-
-
-      // Render each ColumnState
-      for c in s.columns.iter() {
-        let column = gtk::Label::new(None);
-        column.set_text(&c.value);
-        &split.pack_end(&column, false, false, 5);
-      }
-
-      container.pack_start(&split, false, false, 0);
+      names.push(name);
     }
-    viewport.add(&container);
-    window.add(&viewport);
-    window
+    names
+  }
+
+  pub fn init_split_columns(component: &mut Component, state: &State) -> Vec<Vec<gtk::Label>> {
+    let mut rows: Vec<Vec<gtk::Label>> = vec![];
+    for s in &component.state(&state.timer.read(), &state.general_layout_settings).splits {
+      let mut columns: Vec<gtk::Label> = vec![];
+      for c in s.columns.iter() {
+        let label = gtk::Label::new(None);
+        label.set_text(&c.value);
+        columns.push(label);
+      }
+      rows.insert(s.index as usize, columns);
+    }
+    rows
+  }
+
+  pub fn redraw(&mut self, state: &State) {
+    if state.timer.read().current_phase() == livesplit_core::TimerPhase::Running {
+      let current_split_index = state.timer.read().current_split_index().unwrap();
+      for s in self.component.state(&state.timer.read(), &state.general_layout_settings).splits {
+        if s.index <= current_split_index {
+          let columns = &self.split_columns[s.index];
+          for (index, c) in s.columns.iter().enumerate() {
+            let value = &s.columns[index].value;
+            columns[index].set_text(&value);
+          }
+        }
+      }
+      self.widget.show_all();
+    }
   }
 
   fn default_settings() -> Settings {
@@ -194,7 +143,7 @@ impl Splits {
     let current_split_gradient = Plain(Color { rgba: LinSrgba::new(1.0, 0.5, 0.5, 0.8) });
     let show_column_labels = true;
     let columns: Vec<ColumnSettings> = vec![
-      ColumnSettings { 
+      ColumnSettings {
         name: String::from("Personal Best"),
         start_with: ColumnStartWith::ComparisonTime,
         update_with: ColumnUpdateWith::SplitTime,
