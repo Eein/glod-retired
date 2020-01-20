af172c11737fa87588b2871debb91f1f0da0ddfd
William Volin <me@williamvolin.com>
2020-01-19 21:57:55 -0700

    splits refactor, currently has freezing issue

--- src/components/splits.rs
+++ src/components/splits.rs
@@ -17,197 +17,109 @@ use gtk::{
   LabelExt,
   WidgetExt,
   StyleContextExt,
-  Viewport,
-  AdjustmentExt,
-  ScrolledWindow,
-  ScrolledWindowExt,
   Orientation,
-  NONE_ADJUSTMENT,
 };
 
 use crate::state::State;
 
 pub struct Splits {
   component: Component,
-  split_rows: Vec<gtk::Box>,
-  split_columns: Vec<Vec<gtk::Label>>, // indexes = [row][column]
-  pub widget: gtk::ScrolledWindow,
+  pub widget: gtk::Box,
 }
 
 // https://docs.rs/livesplit-core/0.11.0/livesplit_core/component/splits/index.html
-
-// TODO
-// set_size_request(&self, width: i32, height: i32) on comparisons and delta
 
 impl Splits {
   pub fn new(state: &State) -> Splits {
     let settings = Splits::default_settings();
     let mut component = Component::with_settings(settings.clone());
-    let split_columns = Splits::init_split_columns(&mut component, &state);
-    let split_names = Splits::init_split_names(&mut component, &state);
-    let split_rows = Splits::init_split_rows(&mut component, &state);
 
-    let widget = ScrolledWindow::new(NONE_ADJUSTMENT, NONE_ADJUSTMENT);
-    widget.set_min_content_width(300);
-    widget.set_overlay_scrolling(true);
-    widget.set_policy(gtk::PolicyType::Never, gtk::PolicyType::Automatic);
+    let widget = gtk::Box::new(Orientation::Vertical, 0);
     widget.set_hexpand(true);
     widget.set_vexpand(true);
+    gtk::WidgetExt::set_name(&widget, "splits-container");
+    widget.get_style_context().add_class("splits-container");
 
-    let viewport = Viewport::new(NONE_ADJUSTMENT, NONE_ADJUSTMENT);
-    let container = gtk::Box::new(Orientation::Vertical, 0);
-    gtk::WidgetExt::set_name(&container, "splits-container");
-    container.get_style_context().add_class("splits-container");
-
-    for (index, r) in split_rows.iter().enumerate() {
-
-      if index % 2 == 0 {
-        r.get_style_context().add_class("even");
-      } else {
-        r.get_style_context().add_class("odd");
-      }
-
-      let name = &split_names[index];
-      let columns = &split_columns[index];
-
-      r.add(name);
-
-      for c in columns {
-        r.add(c);
-      }
-      container.add(r);
+    let splits = Splits::draw_splits(&mut component, &state);
+    for s in splits {
+      widget.add(&s);
     }
-
-    viewport.add(&container);
-    widget.add(&viewport);
 
     Splits {
       component,
       widget,
-      split_rows,
-      split_columns,
     }
   }
 
-  pub fn init_split_rows(component: &mut Component, state: &State) -> Vec<gtk::Box> {
+  pub fn draw_splits(component: &mut Component, state: &State) -> Vec<gtk::Box> {
     let mut rows: Vec<gtk::Box> = Vec::new();
-    for _s in &component.state(&state.timer.read(), &state.general_layout_settings).splits {
+
+    for s in &component.state(&state.timer.read(), &state.general_layout_settings).splits {
       let row = gtk::Box::new(Orientation::Horizontal, 0);
       row.get_style_context().add_class("split-container");
       row.set_hexpand(true);
-      rows.push(row);
-    }
-    rows
-  }
 
-  pub fn init_split_names(component: &mut Component, state: &State) -> Vec<gtk::Label> {
-    let mut names: Vec<gtk::Label> = vec![];
-    for s in &component.state(&state.timer.read(), &state.general_layout_settings).splits {
+      // set row name
       let name = gtk::Label::new(None);
       name.set_text(&s.name);
       name.set_halign(gtk::Align::Start);
       name.set_hexpand(true);
-      names.push(name);
-    }
-    names
-  }
+      row.add(&name);
 
-  pub fn init_split_columns(component: &mut Component, state: &State) -> Vec<Vec<gtk::Label>> {
-    let mut rows: Vec<Vec<gtk::Label>> = vec![];
-    for s in &component.state(&state.timer.read(), &state.general_layout_settings).splits {
-      let mut columns: Vec<gtk::Label> = vec![];
+      // create column boxes
       for c in s.columns.iter() {
         let label = gtk::Label::new(None);
+        label.get_style_context().add_class("split-column");
+
+        let semantic_color = match c.semantic_color {
+          SemanticColor::Default => "default",
+          SemanticColor::AheadGainingTime => "ahead-gaining-time",
+          SemanticColor::AheadLosingTime => "ahead-losing-time",
+          SemanticColor::BehindLosingTime => "behind-losing-time",
+          SemanticColor::BehindGainingTime => "behind-gaining-time",
+          SemanticColor::BestSegment => "best-segment",
+          SemanticColor::NotRunning => "not-running",
+          SemanticColor::Paused => "paused",
+          SemanticColor::PersonalBest => "personal-best",
+        };
+
         label.set_text(&c.value);
-        label.get_style_context().add_class("column");
-        columns.push(label);
+        label.get_style_context().add_class(semantic_color);
+        label.set_halign(gtk::Align::End);
+        row.add(&label);
+      };
+
+      if s.index % 2 == 0 {
+        row.get_style_context().add_class("even");
+      } else {
+        row.get_style_context().add_class("odd");
       }
-      rows.insert(s.index as usize, columns);
+      if state.timer.read().current_phase() == livesplit_core::TimerPhase::Running {
+        let current_split_index = state.timer.read().current_split_index().unwrap();
+        if s.index == current_split_index {
+          row.get_style_context().add_class("current-split");
+        }
+      }
+      rows.push(row);
     }
+
     rows
   }
 
   pub fn redraw(&mut self, state: &State) {
-    if state.timer.read().current_phase() == livesplit_core::TimerPhase::Running {
-      let current_split_index = state.timer.read().current_split_index().unwrap();
-      for s in self.component.state(&state.timer.read(), &state.general_layout_settings).splits {
-        &self.split_rows[s.index].get_style_context().remove_class("current-split");
-        if s.index == current_split_index {
-          &self.split_rows[s.index].get_style_context().add_class("current-split");
-
-
-          // get height of row
-          // current split * height
-          // if height is greater than current viewport set the scrollbar to height + 1 sizing
-          //
-          // current viewport height:
-          // if let Some(adj) = &self.widget.get_vadjustment() {
-          //   let row_height = &self.split_rows[s.index].get_allocated_height();
-          //   let current_space_taken = current_split_index as i32 * row_height;
-          //   let viewport_height = adj.get_page_size();
-          //   println!("Row Height: {:?}", row_height);
-          //   println!("Current Space Taken: {:?}", current_space_taken);
-          //   println!("Viewport Height: {:?}", viewport_height);
-          //   if (current_space_taken + row_height) > viewport_height as i32 {
-          //     // new position
-
-          //     let over_by = (current_space_taken + row_height) as f64 - viewport_height;
-          //     adj.set_value(over_by);
-          //   }
-          // };
-        }
-        let columns = &self.split_columns[s.index];
-        for (index, c) in s.columns.iter().enumerate() {
-          let value = &c.value;
-          // there has to be a better way...
-          columns[index].get_style_context().remove_class("default");
-          columns[index].get_style_context().remove_class("ahead-gaining-time");
-          columns[index].get_style_context().remove_class("ahead-losing-time");
-          columns[index].get_style_context().remove_class("behind-losing-time");
-          columns[index].get_style_context().remove_class("behind-gaining-time");
-          columns[index].get_style_context().remove_class("best-segment");
-          columns[index].get_style_context().remove_class("not-running");
-          columns[index].get_style_context().remove_class("paused");
-          columns[index].get_style_context().remove_class("personal-best");
-          let semantic_color = match c.semantic_color {
-            SemanticColor::Default => "default",
-            SemanticColor::AheadGainingTime => "ahead-gaining-time",
-            SemanticColor::AheadLosingTime => "ahead-losing-time",
-            SemanticColor::BehindLosingTime => "behind-losing-time",
-            SemanticColor::BehindGainingTime => "behind-gaining-time",
-            SemanticColor::BestSegment => "best-segment",
-            SemanticColor::NotRunning => "not-running",
-            SemanticColor::Paused => "paused",
-            SemanticColor::PersonalBest => "personal-best",
-          };
-          columns[index].set_text(&value);
-          columns[index].get_style_context().add_class(semantic_color);
-          columns[index].set_halign(gtk::Align::End);
-        }
-      }
-      // checking width allocation to make deltas look nice
-      let mut width = -1;
-      for s in 0..self.split_rows.len() - 1 {
-        for c in &self.split_columns[s] {
-          let column_width = c.get_allocated_width();
-          if  column_width > width {
-            width = column_width;
-          }
-        }
-      }
-      for s in 0..self.split_rows.len() - 1 {
-        for c in &self.split_columns[s] {
-          c.set_xalign(width as f32);
-          c.set_size_request(width, -1);
-        }
-      }
-      self.widget.show_all();
+    for c in self.widget.get_children() {
+      self.widget.remove(&c);
     }
+    let splits = Splits::draw_splits(&mut self.component, &state);
+    for s in splits {
+      self.widget.add(&s);
+    }
+    self.widget.show_all();
   }
 
   fn default_settings() -> Settings {
     let background = Same(Plain(Color { rgba: LinSrgba::new(1.0, 0.5, 0.5, 0.8) }));
-    let visual_split_count = 0;
+    let visual_split_count = 8;
     let split_preview_count = 0;
     let show_thin_separators = true;
     let separator_last_split = true;
--- src/state.rs
+++ src/state.rs
@@ -1,13 +1,15 @@
-use livesplit_core::{SharedTimer, Timer, GeneralLayoutSettings};
+use livesplit_core::{SharedTimer, Timer, GeneralLayoutSettings, HotkeyConfig, HotkeySystem};
 use crate::util::parser::SplitParser;
 
 use livesplit_core::parking_lot::RwLock;
 use std::sync::{Arc};
 use crate::config::Config;
-use livesplit_core::TimerPhase::{Running, NotRunning, Paused, Ended};
+use livesplit_hotkey::linux::KeyCode;
+
 
 pub struct State {
   pub timer: SharedTimer,
+  pub hotkeys: HotkeySystem,
   pub general_layout_settings: GeneralLayoutSettings,
 }
 
@@ -15,29 +17,31 @@ impl State {
 
   pub fn new() -> State {
     let run = SplitParser.load();
-    let timer = Arc::new(RwLock::new(Timer::new(run).expect("Run with at least one segment provided")));
-    let general_layout_settings: GeneralLayoutSettings = Config::default_config();
+    let timer: SharedTimer = Arc::new(RwLock::new(Timer::new(run).expect("Run with at least one segment provided")));
 
-    {
-      let shared_timer = timer.clone();
-      std::thread::spawn(||{
-        let hook = livesplit_hotkey::linux::Hook::new().unwrap();
-        hook.register(livesplit_hotkey::KeyCode::AltR, move || {
-          let phase = shared_timer.write().current_phase();
-          match phase {
-            Running => shared_timer.write().split(),
-            NotRunning => shared_timer.write().toggle_pause_or_start(),
-            Paused => shared_timer.write().toggle_pause_or_start(),
-            Ended => shared_timer.write().reset(true),
-          }
-        }).unwrap();
-        std::thread::park();
-      });
-    }
+    let hotkeys = HotkeySystem::with_config(timer.clone(), State::hotkey_config())
+      .expect("hotkeys could not register");
+
+    let general_layout_settings: GeneralLayoutSettings = Config::default_config();
 
     State {
       timer,
+      hotkeys,
       general_layout_settings,
+    }
+  }
+
+  pub fn hotkey_config() -> HotkeyConfig {
+    HotkeyConfig {
+      split: Some(KeyCode::AltR),
+      reset: Some(KeyCode::BackSpace),
+      undo: Some(KeyCode::Up),
+      skip: Some(KeyCode::Down),
+      pause: Some(KeyCode::Pause),
+      undo_all_pauses: Some(KeyCode::SuperL),
+      previous_comparison: Some(KeyCode::Left),
+      next_comparison: Some(KeyCode::Right),
+      toggle_timing_method: Some(KeyCode::Plus),
     }
   }
 }
